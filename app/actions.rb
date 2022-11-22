80bc74d862ad059c8d7b5aa5811b2e398b906766
krose99 <kylaedejer@gmail.com>
2022-11-22 04:36:16 +0000

    add login and logout routes

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,25 @@
+# CONTROLLER
+# Actions/Routes 
+
+helpers do 
+  def current_user 
+    User.find_by(id: session[:user_id])
+  end 
+end 
+
+# Handle an HTTP GET request for the path '/'
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
-end
+end 
 
+# Handle an HTTP GET request for the path '/signup'
 get '/signup' do 
   @user = User.new
   erb (:signup)
 end
 
+# Handle an HTTP POST request for the path '/signup'
 post '/signup' do
   
   email = params[:email]
@@ -18,12 +30,41 @@ post '/signup' do
   @user = User.new ({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
 
   else
     erb(:signup)
   end
 
 end
+
+# Handle an HTTP GET request for the path '/login'
+get '/login' do
+  erb(:login)
+end
+
+
+# Handle an HTTP POST request for the path '/login'
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+  
+  @user = User.find_by(username: username)
+ 
+  if @user && @user.password == password
+    session[:user_id] = @user.id
+    redirect to('/')
+    else 
+      @error_message ="Login failed."
+      erb(:login)
+    end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end 
+
+
 
 
